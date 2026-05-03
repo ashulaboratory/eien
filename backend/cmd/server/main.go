553e9df5ec06b500ecd6a81255f0a85bfa596af0
AshisuMurakami <acepokemo@icloud.com>
2026-05-03 22:28:14 +0900

    feat:main.go

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/api/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "pong",
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
